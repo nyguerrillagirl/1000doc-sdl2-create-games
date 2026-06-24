0b37a55db04d12ca61f8a28929228080d157d315
nyguerrillagirl <nyguerrillagirl@brainycode.com>
2026-06-24 07:09:31 -0400

    "Day #634 - shooter03 - moving the player/ship"

--- parallel_realities/shooter/shooter01/src/input.c
+++ parallel_realities/shooter/shooter01/src/input.c
@@ -2,6 +2,60 @@
 
 #include "input.h"
 
+extern App app;
+
+static void doKeyUp(SDL_KeyboardEvent *event)
+{
+	if (event->repeat == 0)
+	{
+		if (event->keysym.scancode == SDL_SCANCODE_UP)
+		{
+			app.up = 0;
+		}
+
+		if (event->keysym.scancode == SDL_SCANCODE_DOWN)
+		{
+			app.down = 0;
+		}
+
+		if (event->keysym.scancode == SDL_SCANCODE_LEFT)
+		{
+			app.left = 0;
+		}
+
+		if (event->keysym.scancode == SDL_SCANCODE_RIGHT)
+		{
+			app.right = 0;
+		}
+	}
+}
+
+static void doKeyDown(SDL_KeyboardEvent *event)
+{
+	if (event->repeat == 0)
+	{
+		if (event->keysym.scancode == SDL_SCANCODE_UP)
+		{
+			app.up = 1;
+		}
+
+		if (event->keysym.scancode == SDL_SCANCODE_DOWN)
+		{
+			app.down = 1;
+		}
+
+		if (event->keysym.scancode == SDL_SCANCODE_LEFT)
+		{
+			app.left = 1;
+		}
+
+		if (event->keysym.scancode == SDL_SCANCODE_RIGHT)
+		{
+			app.right = 1;
+		}
+	}
+}
+
 void doInput(void)
 {
 	SDL_Event event;
@@ -12,6 +66,14 @@ void doInput(void)
 		{
 			case SDL_QUIT:
 				exit(0);
+				break;
+
+			case SDL_KEYDOWN:
+				doKeyDown(&event.key);
+				break;
+
+			case SDL_KEYUP:
+				doKeyUp(&event.key);
 				break;
 
 			default:
